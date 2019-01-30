09ccbb5cefa70d8d514d04f08b33bdcc1cde8fb6
martin <martinleroy@msn.com>
2019-01-30 19:04:42 +0100

    test heroku

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from resources.exchangeocurence import ExchangeOcurence, ExchangeOcurenceList
 from resources.message import Message
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(seconds=7200)
@@ -28,4 +31,10 @@ api.add_resource(Message, '/message/<int:id>')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
     app.run(port=5000, debug=True)
