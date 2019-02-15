522ed28a52c1d222909793cbea076610d10c0ab2
martin <martinleroy@msn.com>
2019-02-15 23:09:25 +0100

    fix bug indent

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
 
-     if app.config['DEBUG']:
+    if app.config['DEBUG']:
         @app.before_first_request
         def create_tables():
             db.create_all()
 
-    app.run(port=5000, debug=True)
+    app.run(port=5000)
