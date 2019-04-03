f01c0a0f80ebb4f6d4c1cf4b86c1416a1a41f7a9
martin <martinleroy@msn.com>
2019-04-03 18:25:33 +0200

    change string long

--- models/user.py
+++ models/user.py
@@ -15,7 +15,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80))
-    password = db.Column(db.String(80))
+    password = db.Column(db.String(256))
     email = db.Column(db.String(80))
     counterHours = db.Column(db.Integer)
 
